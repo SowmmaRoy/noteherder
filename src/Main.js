b74fe507e1086164962d7ed30e78321e6d963606
Sowmma Roy <sroy15@earlham.edu>
2017-07-06 18:18:46 -0400

    Add Main component

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/*import './Main.css'
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'*/
+
+const Main = () => {
+  return <p>Main!</p>
+
+
+}
+
+export default Main 
