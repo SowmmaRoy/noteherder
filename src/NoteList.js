fb8559d478aa9bf05c22d70b1e62fa6468fa7e96
Sowmma Roy <sroy15@earlham.edu>
2017-07-09 16:32:40 -0400

    Store (hard-coded) notes in App's state

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,27 +3,22 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = () => {
-    const notes = [
-        {
-            id: 'note-4',
-            title: 'My fancy note',
-            body: 'Oh so fancy, I do declare!',
-        },
-        {
-            id: 'note-5',
-            title: 'Another one',
-            body: 'Also very fancy'
-        }
-    ]
-    return (
-        <div className="NoteList">
-            <h3>Notes</h3>
-            <ul id="notes">
-              {notes.map(note => <Note note={note}/>)}                              
-            </ul>
-        </div>
-    )
+const NoteList = (props) => {
+
+// ['note-4', 'note-5']
+const noteIds = Object.keys(props.notes)
+
+ 
+return (
+    <div className="NoteList">
+        <h3>Notes</h3>
+        <ul id="notes">
+        {notesIds.map(noteId => (
+            <Note note={props.notes[noteId]}/>
+        ))}                              
+        </ul>
+    </div>
+  )
 }
 
 export default NoteList
